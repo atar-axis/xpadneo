726078c67a26cc073129ec191140629ad9b02b7b
Florian Dollinger <atar-axis@users.noreply.github.com>
2018-03-01 21:37:47 +0100

    force root and check crc32 of installed modules

--- src/install.sh
+++ src/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 hid_xpadneo=$(modinfo -n hid-xpadneo 2> /dev/null)
 hid=$(modinfo -n hid 2> /dev/null)
 bluetooth=$(modinfo -n bluetooth 2> /dev/null)
@@ -13,10 +18,10 @@ sudo [ ! -d "$extramodules"_bak ] && sudo mkdir --parents $extramodules
 
 if [ "$hid_xpadneo" == "" ]
 then
-  echo "* install xpadneo"
+  echo "** install xpadneo"
   sudo cp -f ./hid-xpadneo.ko "$extramodules"
 else
-  echo "* replace xpadneo"
+  echo "** replace xpadneo"
   sudo [ ! -f "$hid_xpadneo"_bak ] && sudo mv "$hid_xpadneo" "$hid_xpadneo"_bak
   sudo cp -f ./hid-xpadneo.ko $(dirname "$hid_xpadneo")
 fi
@@ -25,10 +30,10 @@ fi
 
 if [ "$hid" == "" ]
 then
-  echo "* install hid"
+  echo "** install hid"
   sudo cp -f ./hid.ko "$extramodules"
 else
-  echo "* replace hid"
+  echo "** replace hid"
   sudo [ ! -f "$hid"_bak ] && sudo mv "$hid" "$hid"_bak
   sudo cp -f ./hid.ko $(dirname "$hid")
 fi
@@ -37,13 +42,23 @@ fi
 
 if [ "$bluetooth" == "" ]
 then
-  echo "* install bluetooth"
+  echo "** install bluetooth"
   sudo cp -f ./bluetooth.ko "$extramodules"
 else
-  echo "* replace bluetooth"
+  echo "** replace bluetooth"
   sudo [ ! -f "$bluetooth"_bak ] && sudo mv "$bluetooth" "$bluetooth"_bak
   sudo cp -f ./bluetooth.ko $(dirname "$bluetooth")
 fi
+
+if [[ $(crc32 $hid) != $(crc32 ./hid.ko) \
+   || $(crc32 $bluetooth) != $(crc32 ./bluetooth.ko) \
+   || $(crc32 $hid_xpadneo) != $(crc32 ./hid-xpadneo.ko) ]]
+then
+  echo something went wrong, files checksums are not correct
+  exit 1
+else 
+  echo "* installed/replaced successfully"
+fi;
 
 
 sudo depmod
