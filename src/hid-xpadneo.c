7051a8f07cfae96ea5db265c45dcfb39afbd5819
atar-axis <dollinger.florian@gmx.de>
2018-03-07 17:12:02 +0100

    added startup input events, fixes #20

--- src/hid-xpadneo.c
+++ src/hid-xpadneo.c
@@ -200,7 +200,7 @@ static int xpadneo_initDevice(struct hid_device *hdev)
 	 * TODO: replace by get_drvdata
 	 */
 	struct hid_input *hidinput = list_entry(hdev->inputs.next, struct hid_input, list);
-	struct input_dev *dev      = hidinput->input;
+	struct input_dev *idev      = hidinput->input;
 
 	struct ff_report ff_package;
 
@@ -225,11 +225,41 @@ static int xpadneo_initDevice(struct hid_device *hdev)
 
 
 	/* Init Input System for Force Feedback (FF) */
-	input_set_capability(dev, EV_FF, FF_RUMBLE);
-	error = input_ff_create_memless(dev, NULL, xpadneo_ff_play);
+	input_set_capability(idev, EV_FF, FF_RUMBLE);
+	error = input_ff_create_memless(idev, NULL, xpadneo_ff_play);
 	if (error) {
 		return error;
 	}
+
+
+	/*
+	 * Set default values, otherwise tools which depend on the joystick
+	 * subsystem, report arbitrary values until the first real event 
+	 */
+	input_report_abs(idev, ABS_X, 32768);
+	input_report_abs(idev, ABS_Y, 32768);
+	input_report_abs(idev, ABS_Z, 0);
+	input_report_abs(idev, ABS_RX, 32768);
+	input_report_abs(idev, ABS_RY, 32768);
+	input_report_abs(idev, ABS_RZ, 0);
+	input_report_key(idev, BTN_A, 0);
+	input_report_key(idev, BTN_B, 0);
+	input_report_key(idev, BTN_X, 0);
+	input_report_key(idev, BTN_Y, 0);
+	input_report_key(idev, BTN_TR, 0);
+	input_report_key(idev, BTN_TL, 0);
+	input_report_key(idev, BTN_THUMBL, 0);
+	input_report_key(idev, BTN_THUMBR, 0);
+	input_report_key(idev, BTN_START, 0);
+	input_report_key(idev, BTN_MODE, 0);
+	input_report_key(idev, ABS_HAT0X, 0);
+	input_report_key(idev, ABS_HAT0X, 1);
+	input_sync(idev);
+
+	/* TODO: - do not hardcode codes and values but
+	 *         keep them in the mapping structures
+	 *       - maybe initDevice isn't the right place
+	 */
 
 	return 0;
 }
