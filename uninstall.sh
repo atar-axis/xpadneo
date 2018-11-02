08e30522596f22c9bc47712e98f37e9b53d35339
atar-axis <dollinger.florian@gmx.de>
2018-11-02 19:23:18 +0100

    removed sudo in uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,11 +2,17 @@
 
 source VERSION
 
+if (( EUID != 0 )); then
+  echo "ERROR: You most probably need superuser privileges to uninstall modules, please run me via sudo!"
+  exit -3
+fi
+
+
 echo "* unloading driver module"
-sudo modprobe -r hid_xpadneo
+modprobe -r hid_xpadneo
 
 echo "* uninstalling and removing from DKMS"
-sudo dkms remove -m hid-xpadneo -v $VERSION --all
+dkms remove -m hid-xpadneo -v $VERSION --all
 
 echo "* removing folder from /usr/src"
-sudo rm --recursive /usr/src/hid-xpadneo-$VERSION/
+rm --recursive /usr/src/hid-xpadneo-$VERSION/
