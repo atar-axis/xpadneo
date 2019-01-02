cb162579da6868ab512d3593e67660c2813e81d2
atar-axis <dollinger.florian@gmx.de>
2019-01-02 14:02:46 +0100

    added update script

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# exit immediately if one command fails
+set -e 
+
+source VERSION
+
+if [[ $EUID != 0 ]]; then
+  echo "ERROR: You most probably need superuser privileges to update modules, please run me via sudo!"
+  exit -3
+fi
+
+
+echo "* uninstalling the outdated module"
+./uninstall.sh
+
+echo "* updating the git clone"
+git pull
+
+echo "* installing updated module"
+./install
