af5def110918b5479949b2f9e1409e5f190b3ee3
atar-axis <dollinger.florian@gmx.de>
2019-01-02 15:30:13 +0100

    check not only the directory but the installed version too

--- update.sh
+++ update.sh
@@ -6,19 +6,33 @@ if [[ $EUID != 0 ]]; then
 fi
 
 LATEST=$(wget --quiet --output-document=/dev/stdout "https://raw.githubusercontent.com/atar-axis/xpadneo/master/VERSION" | sed -E 's/.*"([0-9]*.[0-9]*.[0-9]*)".*/\1/')
+INSTALLED=$(dkms status 2>/dev/null | grep '^hid-xpadneo' 2>/dev/null | sed -E 's/^hid-xpadneo, ([0-9]+.[0-9]+.[0-9]+).*installed/\1/')
 source VERSION
 
+IS_GIT=$(git rev-parse --is-inside-work-tree 2>/dev/null)
 
-if [[ $LATEST == $VERSION ]]
-then
-    echo "You already have the latest version! Yay."
+
+if [[ $LATEST == $VERSION ]]; then
+    echo "This directory is up to date!"
 else
-    echo "* uninstalling the outdated module"
-    ./uninstall.sh
-
     echo "* updating the cloned git directory"
-    git pull
+    
+    if [[ $IS_GIT == "true" ]]; then
+        git fetch --all
+        git reset --hard HEAD
+    else
+        echo "Error, cannot update since this is not a Git repo (please update by hand)."
+        exit -4
+    fi
+    
+fi
 
-    echo "* installing updated module"
+if [[ $LATEST == $INSTALLED ]]; then
+    echo "You have already installed the latest version! Yay."
+else
+    echo "* uninstalling outdated modules"
+    ./uninstall.sh
+        
+    echo "* installing latest version"
     ./install.sh
 fi
