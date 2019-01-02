aeb7835e2a2f152533fca07364acff1c331e2cf2
Florian Dollinger <atar-axis@users.noreply.github.com>
2019-01-02 17:09:28 +0100

    Update script (#65)
    * added update script
    
    * changed uninstall script to remove all versions of hid-xpadneo, see #64
    
    * minor changes
    
    * fixed update script, added version check before downloading a new one, #64
    
    * check not only the directory but the installed version too
    
    * removed automatic git update, added instructions instead.
    
    * updated version to 0.5.1
    
    * temporarily changed the VERSION path for testing
    
    * revert the VERSION path

--- uninstall.sh
+++ uninstall.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-source VERSION
-
 if [[ $EUID != 0 ]]; then
   echo "ERROR: You most probably need superuser privileges to uninstall modules, please run me via sudo!"
   exit -3
 fi
 
 
-echo "* unloading driver module"
+echo "* unloading current driver module"
 modprobe -r hid_xpadneo
 
-echo "* uninstalling and removing from DKMS"
-dkms remove -m hid-xpadneo -v $VERSION --all
+echo "* looking for registered instances"
+VER=($(dkms status 2>/dev/null | grep '^hid-xpadneo' 2>/dev/null | sed -E 's/^hid-xpadneo, ([0-9]+.[0-9]+.[0-9]+).*/\1/'))
+echo "found ${#VER[@]} registered instance(s) on your system"
 
-echo "* removing folder from /usr/src"
-rm --recursive /usr/src/hid-xpadneo-$VERSION/
+
+for instance in "${VER[@]}"
+do
+    echo "* $instance"
+
+    echo "  * uninstalling and removing $instance from DKMS"
+    dkms remove -m hid-xpadneo -v $instance --all
+
+    echo "  * removing $instance folder from /usr/src"
+    rm --recursive /usr/src/hid-xpadneo-$instance/
+done
---
+++ update.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [[ $EUID != 0 ]]; then
+  echo "ERROR: You most probably need superuser privileges to update modules, please run me via sudo!"
+  exit -3
+fi
+
+LATEST=$(wget --quiet --output-document=/dev/stdout "https://raw.githubusercontent.com/atar-axis/xpadneo/master/VERSION" | sed -E 's/.*"([0-9]*.[0-9]*.[0-9]*)".*/\1/')
+INSTALLED=$(dkms status 2>/dev/null | grep '^hid-xpadneo' 2>/dev/null | sed -E 's/^hid-xpadneo, ([0-9]+.[0-9]+.[0-9]+).*installed/\1/')
+source VERSION
+
+IS_GIT=$(git rev-parse --is-inside-work-tree 2>/dev/null)
+
+
+if [[ $LATEST == $VERSION ]]; then
+
+    echo "Looks like the repo is up to date, fine!"
+    
+    if [[ $LATEST == $INSTALLED ]]; then
+        echo "You have already installed the latest version! Yay."
+    else
+        echo "* uninstalling outdated modules"
+        ./uninstall.sh
+            
+        echo "* installing latest version"
+        ./install.sh
+    fi
+    
+else
+    
+    if [[ $IS_GIT == "true" ]]; then
+        echo "Please update this directory by running git pull, afterwards run this script again"
+    else
+        echo "Please update this directory by downloading the latest version from https://github.com/atar-axis/xpadneo/archive/master.zip"
+    fi
+    
+    exit -4
+
+fi
+
+
