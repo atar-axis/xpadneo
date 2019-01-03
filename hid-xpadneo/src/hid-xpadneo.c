86293aa228815a58ae5acc080e06a5125db2b0b8
atar-axis <dollinger.florian@gmx.de>
2019-01-03 17:33:03 +0100

    changed dummy string to @DO_NOT_CHANGE@

--- hid-xpadneo/src/hid-xpadneo.c
+++ hid-xpadneo/src/hid-xpadneo.c
@@ -1,4 +1,4 @@
-#define DRV_VER "DUMMY"
+#define DRV_VER "@DO_NOT_CHANGE@"
 
 /*
  * Force feedback support for XBOX ONE S and X gamepads via Bluetooth
--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ fi
 
 
 echo "* replacing version string if necessary"
-sed -i 's/PACKAGE_VERSION="DUMMY"/PACKAGE_VERSION="'$VERSION'"/g' hid-xpadneo/dkms.conf
-sed -i 's/#define DRV_VER "DUMMY"/#define DRV_VER "'$VERSION'"/g' hid-xpadneo/src/hid-xpadneo.c
+sed -i 's/PACKAGE_VERSION="@DO_NOT_CHANGE@"/PACKAGE_VERSION="'$VERSION'"/g' hid-xpadneo/dkms.conf
+sed -i 's/#define DRV_VER "@DO_NOT_CHANGE@"/#define DRV_VER "'$VERSION'"/g' hid-xpadneo/src/hid-xpadneo.c
 
 
 INSTALLED=$(dkms status 2>/dev/null | grep '^hid-xpadneo' 2>/dev/null | sed -E 's/^hid-xpadneo, ([0-9]+.[0-9]+.[0-9]+).*installed/\1/')
