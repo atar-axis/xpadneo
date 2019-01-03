c55530ca5aeb258e0aae565d27d6533fa0647ae2
atar-axis <dollinger.florian@gmx.de>
2019-01-03 22:19:32 +0100

    fixed update script

--- update.sh
+++ update.sh
@@ -29,7 +29,7 @@ if [[ $LATEST == $VERSION ]]; then
 else
     
     if [[ $IS_GIT == "true" ]]; then
-        echo "Please update this directory by running `git reset --hard` and `git pull`, afterwards run this script again"
+        echo "Please update this directory by running 'git reset --hard' and 'git pull', afterwards run this script again"
     else
         echo "Please update this directory by downloading the latest version from https://github.com/atar-axis/xpadneo/archive/master.zip"
     fi
