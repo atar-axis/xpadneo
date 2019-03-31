0905f1398f8dbfd00155a00a1953d4f5046bcfa0
atar-axis <dollinger.florian@gmx.de>
2019-03-31 14:49:48 +0200

    v0.5.9 - tweak: added some other quotes to satisfy shellchek

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 # exit immediately if one command fails
 set -e
+set -x
 
 if [[ $EUID != 0 ]]; then
   echo "ERROR: You most probably need superuser privileges to install new modules, please run me via sudo!"
@@ -15,8 +16,8 @@ cp --preserve hid-xpadneo/dkms.conf hid-xpadneo/dkms.conf_bck
 cp --preserve hid-xpadneo/src/hid-xpadneo.c hid-xpadneo/src/hid-xpadneo.c_bck
 
 echo "* replacing version string if necessary"
-sed -i 's/PACKAGE_VERSION="@DO_NOT_CHANGE@"/PACKAGE_VERSION="'$VERSION'"/g' hid-xpadneo/dkms.conf
-sed -i 's/#define DRV_VER "@DO_NOT_CHANGE@"/#define DRV_VER "'$VERSION'"/g' hid-xpadneo/src/hid-xpadneo.c
+sed -i 's/PACKAGE_VERSION="@DO_NOT_CHANGE@"/PACKAGE_VERSION="'"$VERSION"'"/g' hid-xpadneo/dkms.conf
+sed -i 's/#define DRV_VER "@DO_NOT_CHANGE@"/#define DRV_VER "'"$VERSION"'"/g' hid-xpadneo/src/hid-xpadneo.c
 
 
 INSTALLED=$(dkms status 2>/dev/null | grep '^hid-xpadneo,' 2>/dev/null | sed -E 's/^hid-xpadneo, ([0-9]+.[0-9]+.[0-9]+).*installed/\1/')
@@ -24,7 +25,7 @@ INSTALLED=$(dkms status 2>/dev/null | grep '^hid-xpadneo,' 2>/dev/null | sed -E 
 if [[ -z "$INSTALLED" ]]; then
 
     echo "* copying module into /usr/src"
-    cp --recursive "$PWD/hid-xpadneo/ /usr/src/hid-xpadneo-$VERSION"
+    cp --recursive "$PWD/hid-xpadneo/" "/usr/src/hid-xpadneo-$VERSION"
 
     echo "* adding module to DKMS"
     dkms add -m hid-xpadneo -v "$VERSION"
