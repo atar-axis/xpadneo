73e7e44a892cf91376c2758fb94e724a89820c42
atar-axis <dollinger.florian@gmx.de>
2019-05-22 16:11:38 +0200

    * fixed: wrong divider

--- hid-xpadneo/src/hid-xpadneo.c
+++ hid-xpadneo/src/hid-xpadneo.c
@@ -957,7 +957,7 @@ int xpadneo_event(struct hid_device *hdev, struct hid_field *field,
 				vmouse_movement(0, mouse_value);
 				break;
 			case ABS_RY:
-				mouse_value = -(centered_value / 1024);
+				mouse_value = -(centered_value / 8192);
 				vmouse_wheel(mouse_value);
 				break;
 			}
