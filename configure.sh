atar-axis/xpadneo

commit 43faf20e3bc08fdf1fc5575f19118da02c41339c

* fixes: #114 * fixes: #113 (workaround, introduced a fake_pid option

configure.sh
@@ -23,7 +23,7 @@ PARAMS="/sys/module/hid_xpadneo/parameters"
 CONF_FILE=$(find /etc/modprobe.d/ -mindepth 1 -maxdepth 1 -type f -name "*xpadneo*")
 
 # Use getopt NOT getopts to parse arguments.
-OPTS=$(getopt -n "$NAME" -o hz:d:f:v:r: -l help,version,combined-z-axis:,debug-level:,disable-ff:,fake-dev-version:,trigger-rumble-damping: -- "$@")
+OPTS=$(getopt -n "$NAME" -o hz:d:f:v:p:r: -l help,version,combined-z-axis:,debug-level:,disable-ff:,fake-usb-pid:,fake-dev-version:,trigger-rumble-damping: -- "$@")
 
 
 
@@ -41,7 +41,7 @@ function display_version {
 function check_param {
     key=$1
     value=$2
-    
+
     case $key in
     "debug_level")
         if [[ "$value" -gt 3 ]] || [[ "$value" -lt 0 ]];
@@ -65,9 +65,16 @@ function check_param {
         fi
         ;;
     "fake_dev_version")
-        if [[ "$value" -gt 65535 ]] || [[ "$value" -lt 1 ]];
+        if [[ "$value" -gt 65535 ]] || [[ "$value" -lt 0 ]];
         then
-            echo "$NAME: $key: Invalid value! Value must be between 1 and 65535."
+            echo "$NAME: $key: Invalid value! Value must be between 0 and 65535."
+            exit 1
+        fi
+        ;;
+    "fake_usb_pid")
+        if [[ "$value" != "y" ]] && [[ "$value" != "n" ]];
+        then
+            echo "$NAME: $key: Invalid value! Value must be 'y' or 'n'."
             exit 1
         fi
         ;;
@@ -100,7 +107,7 @@ function set_param {
 
   key=$1
   value=$2
-  
+
   # check for valid parameters first
   check_param "$key" "$value"
 
@@ -114,7 +121,7 @@ function set_param {
       exit 1
     fi
   fi
-  
+
   # edit modprobe config file
   if ! set_modprobe_param "$key" "$value";
   then
@@ -122,7 +129,7 @@ function set_param {
     exit 1
   fi
   echo "$NAME: $key: parameter written to $CONF_FILE"
-  
+
 }
 
 ## Argument Parsing ##
@@ -131,7 +138,7 @@ function parse_args {
   if [[ -z "$LINE_EXISTS" ]];
   then
     # If line doesn't exist echo all of the defaults.
-    echo "options hid_xpadneo debug_level=0 disable_ff=0 trigger_rumble_damping=4 fake_dev_version=4400 combined_z_axis=n" >> "$CONF_FILE"
+    echo "options hid_xpadneo debug_level=0 disable_ff=0 trigger_rumble_damping=4 fake_dev_version=4400 fake_usb_pid=n combined_z_axis=n" >> "$CONF_FILE"
   fi
 
   if [[ $1 == "" ]];
@@ -178,6 +185,13 @@ function parse_args {
 
       -v | --fake-dev-version)
         key='fake_dev_version'
+        value="${2#*=}"
+        set_param "$key" "$value"
+        shift 2
+        ;;
+
+      -p | --fake-usb-pid)
+        key='fake_usb_pid'
         value="${2#*=}"
         set_param "$key" "$value"
         shift 2

docs/config_help
@@ -18,10 +18,13 @@ NOTE: Short options don't require an equals sign, only long options.  EX. -d 3 v
 	Damp the strength of the trigger force feedback. [1 (none) to 256 (max)]
 
 -v, --fake-dev-version
-	Fake the input device version to the given value. [1 to 65535]
+	Fake the input device version to the given value. [0 to 65535]
+
+-p, --fake-usb-pid
+	Fake the input device version to 0x02E0, which is the one which is used for a USB connection. [y, n]
 
 -z, --combined-z-axis
-	Combine the triggers (ABS_Z and ABS_RZ) to form a single axis ABS_Z which is used e.g. in 
+	Combine the triggers (ABS_Z and ABS_RZ) to form a single axis ABS_Z which is used e.g. in
 	flight simulators.  The left and right trigger will work against each other.  [y, n]
 
 

hid-xpadneo/src/hid-xpadneo.c
@@ -62,6 +62,11 @@ static u16 param_fake_dev_version = 0x1130;
 module_param_named(fake_dev_version, param_fake_dev_version, ushort, 0644);
 MODULE_PARM_DESC(fake_dev_version, "(u16) Fake device version # to hide from SDL's mappings. 0x0001-0xFFFF: fake version, others: keep original");
 
+static bool param_fake_usb_pid;
+module_param_named(fake_usb_pid, param_fake_usb_pid, bool, 0644);
+MODULE_PARM_DESC(fake_usb_pid, "(bool) Fake the product ID to 0x02EA, which is the USB pId.");
+
+
 
 /*
  * Debug Printk
@@ -1037,6 +1042,11 @@ static int xpadneo_input_configured(struct hid_device *hdev,
 		xdata->idev->id.version = (u16) param_fake_dev_version;
 		hid_dbg_lvl(DBG_LVL_FEW, hdev, "Fake device version: 0x%04X\n",
 			param_fake_dev_version);
+	}
+
+	if (param_fake_usb_pid) {
+		xdata->idev->id.product = 0x02ea;
+		hid_dbg_lvl(DBG_LVL_FEW, hdev, "Fake product id: 0x02EA\n");
 	}
 
 

commit 0e81a6cc24ec9e49ae70ec02ea9eaf480d73467f

Pretend we are in Windows wireless mode
This commit removes the option for faking the device version number as
the HTML5 game controller APIs doesn't look at that (in contrast to
SDL which does) and instead pretends we are in Windows wireless mode.

That way, we still expose that the controller is connected in wireless
mode (so it shows up as "Xbox Wireless Controller" in softwares) but
we alleviate the several fixups already implemented in multiple API
layers.

Fixes: commit 43faf20e3bc08fdf1fc5575f19118da02c41339c
Fixes: commit 71adbbb5ac333130bb91a7 ("switched from 0x02EA to 0x02E0")
Fixes: commit 3f3860bc ("Add Support For Xbox Elite Series 2 Wireless")
Fixes: https://github.com/atar-axis/xpadneo/issues/113
Signed-off-by: Kai Krakow <kai@kaishome.de>

configure.sh
@@ -23,7 +23,7 @@ PARAMS="/sys/module/hid_xpadneo/parameters"
 CONF_FILE=$(find /etc/modprobe.d/ -mindepth 1 -maxdepth 1 -type f -name "*xpadneo*")
 
 # Use getopt NOT getopts to parse arguments.
-OPTS=$(getopt -n "$NAME" -o hz:d:f:v:r: -l help,version,combined-z-axis:,debug-level:,disable-ff:,fake-dev-version:,trigger-rumble-damping: -- "$@")
+OPTS=$(getopt -n "$NAME" -o hz:d:f:v:r: -l help,version,combined-z-axis:,debug-level:,disable-ff:,trigger-rumble-damping: -- "$@")
 
 
 
@@ -61,13 +61,6 @@ function check_param {
         if [[ "$value" -gt 256 ]] || [[ "$value" -lt 1 ]];
         then
             echo "$NAME: $key: Invalid value! Value must be between 1 and 256."
-            exit 1
-        fi
-        ;;
-    "fake_dev_version")
-        if [[ "$value" -gt 65535 ]] || [[ "$value" -lt 1 ]];
-        then
-            echo "$NAME: $key: Invalid value! Value must be between 1 and 65535."
             exit 1
         fi
         ;;
@@ -171,13 +164,6 @@ function parse_args {
 
       -r | --trigger-rumble-damping)
         key='trigger_rumble_damping'
-        value="${2#*=}"
-        set_param "$key" "$value"
-        shift 2
-        ;;
-
-      -v | --fake-dev-version)
-        key='fake_dev_version'
         value="${2#*=}"
         set_param "$key" "$value"
         shift 2

docs/config_help
@@ -17,11 +17,8 @@ NOTE: Short options don't require an equals sign, only long options.  EX. -d 3 v
 -r, --trigger-rumble-damping
 	Damp the strength of the trigger force feedback. [1 (none) to 256 (max)]
 
--v, --fake-dev-version
-	Fake the input device version to the given value. [1 to 65535]
-
 -z, --combined-z-axis
-	Combine the triggers (ABS_Z and ABS_RZ) to form a single axis ABS_Z which is used e.g. in 
+	Combine the triggers (ABS_Z and ABS_RZ) to form a single axis ABS_Z which is used e.g. in
 	flight simulators.  The left and right trigger will work against each other.  [y, n]
 
 

hid-xpadneo/src/hid-xpadneo.c
@@ -66,11 +66,6 @@ module_param_named(trigger_rumble_damping,
 MODULE_PARM_DESC(trigger_rumble_damping,
 		 "(u8) Damp the trigger: 1 (none) to 2^8+ (max)");
 
-static u16 param_fake_dev_version = 0x1130;
-module_param_named(fake_dev_version, param_fake_dev_version, ushort, 0644);
-MODULE_PARM_DESC(fake_dev_version,
-		 "(u16) Fake device version # to hide from SDL's mappings. 0x0001-0xFFFF: fake version, others: keep original");
-
 /*
  * Debug Printk
  *
@@ -1027,10 +1022,24 @@ static int xpadneo_input_configured(struct hid_device *hdev,
 
 	hid_dbg_lvl(DBG_LVL_SOME, hdev, "INPUT CONFIGURED HOOK\n");
 
-	if (param_fake_dev_version) {
-		xdata->idev->id.version = (u16)param_fake_dev_version;
-		hid_dbg_lvl(DBG_LVL_FEW, hdev, "Fake device version: 0x%04X\n",
-			    param_fake_dev_version);
+	/*
+	 * Pretend that we are in Windows pairing mode as we are actually
+	 * exposing the Windows mapping. This prevents SDL and other layers
+	 * (probably browser game controller APIs) from treating our driver
+	 * unnecessarily with button and axis mapping fixups, and it seems
+	 * this is actually a firmware mode meant to Android usage only:
+	 * 0x2E0 wireless Windows mode (non-Android mode)
+	 * 0x2EA USB Windows and Linux mode
+	 * 0x2FD wireless Linux mode (Android mode)
+	 */
+	switch (xdata->idev->id.product) {
+	case 0x02FD:
+		hid_info(hdev,
+			 "pretending Windows wireless mode (changed PID from 0x%04X to 0x02E0)\n",
+			 (u16)xdata->idev->id.product);
+		xdata->idev->id.product = 0x02E0;
+		break;
+
 	}
 
 	/*