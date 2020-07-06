e4c69ffe0c294918e413a40cbd846f7f5d60c27d
Kai Krakow <kai@kaishome.de>
2020-07-07 00:07:30 +0200

    dkms, install: Do not apply inline version patching
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- install.sh
+++ install.sh
@@ -3,19 +3,18 @@
 # shellcheck disable=SC1090
 source "$(dirname "$0")/lib/installer.sh"
 
-# backup original files, preserve permissions
-cp --preserve hid-xpadneo/dkms.conf hid-xpadneo/dkms.conf_bck
-cp --preserve hid-xpadneo/src/hid-xpadneo.c hid-xpadneo/src/hid-xpadneo.c_bck
-
-echo "* replacing version string if necessary"
-sed -i 's/PACKAGE_VERSION="@DO_NOT_CHANGE@"/PACKAGE_VERSION="'"$VERSION"'"/g' hid-xpadneo/dkms.conf
-sed -i 's/#define DRV_VER "@DO_NOT_CHANGE@"/#define DRV_VER "'"$VERSION"'"/g' hid-xpadneo/src/hid-xpadneo.c
-
+DESTDIR="/usr/src/hid-xpadneo-${VERSION}"
 
 if [[ -z "$INSTALLED" ]]; then
 
-    echo "* copying module into /usr/src"
-    cp --recursive "$PWD/hid-xpadneo/" "/usr/src/hid-xpadneo-$VERSION"
+    echo "* copying module to '${DESTDIR}'"
+    cp --recursive "hid-xpadneo" "${DESTDIR}"
+
+	echo "* replacing version string if necessary"
+	(
+	 	cd "${DESTDIR}"
+		sed -i 's/"@DO_NOT_CHANGE@"/"'"${VERSION}"'"/g' dkms.conf src/hid-xpadneo.c
+	)
 
     echo "* adding module to DKMS"
     dkms add "hid-xpadneo/${VERSION}"
@@ -28,7 +27,3 @@ else
     echo "already installed!"
 
 fi
-
-# restore original files
-mv hid-xpadneo/dkms.conf_bck hid-xpadneo/dkms.conf
-mv hid-xpadneo/src/hid-xpadneo.c_bck hid-xpadneo/src/hid-xpadneo.c
