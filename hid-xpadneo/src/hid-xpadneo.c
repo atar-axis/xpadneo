b0b586892c71bbe5fbbd73be582cabc9dc98c3c2
Kai Krakow <kai@kaishome.de>
2020-11-11 21:47:51 +0100

    hid-xpadneo: Document XBXS modes and PIDs
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- hid-xpadneo/src/hid-xpadneo.c
+++ hid-xpadneo/src/hid-xpadneo.c
@@ -993,6 +993,10 @@ static int xpadneo_input_configured(struct hid_device *hdev, struct hid_input *h
 	 * Xbox Elite 2:
 	 * 0xB00 USB Windows and Linux mode
 	 * 0xB05 wireless Linux mode (Android mode)
+	 *
+	 * Xbox Series X/S:
+	 * 0xB12 USB Windows and Linux mode
+	 * 0xB13 wireless Linux mode (Android mode)
 	 */
 	switch (xdata->idev->id.product) {
 	case 0x02E0:
