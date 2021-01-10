a63e85095b0dd617d59a30722f3ce511101716ed
Kai Krakow <kai@kaishome.de>
2021-01-10 17:54:32 +0100

    hid-xpadneo: Improve descriptor logging
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- hid-xpadneo/src/hid-xpadneo.c
+++ hid-xpadneo/src/hid-xpadneo.c
@@ -702,11 +702,11 @@ static u8 *xpadneo_report_fixup(struct hid_device *hdev, u8 *rdesc, unsigned int
 	struct xpadneo_devdata *xdata = hid_get_drvdata(hdev);
 
 	xdata->original_rsize = *rsize;
-	hid_notice(hdev, "descriptor size: %d bytes\n", *rsize);
+	hid_info(hdev, "report descriptor size: %d bytes\n", *rsize);
 
 	/* fixup trailing NUL byte */
 	if (rdesc[*rsize - 2] == 0xC0 && rdesc[*rsize - 1] == 0x00) {
-		hid_notice(hdev, "fixing up report size\n");
+		hid_notice(hdev, "fixing up report descriptor size\n");
 		*rsize -= 1;
 	}
 
