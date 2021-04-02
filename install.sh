7fd4f538544f54541010f5e2086d92ec5fb0295c
Kai Krakow <kai@kaishome.de>
2021-04-02 14:51:27 +0200

    dkms: Simplify installation
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- install.sh
+++ install.sh
@@ -4,27 +4,15 @@ cd "$(dirname "$0")" || exit 1
 source "lib/verbose.sh"
 source "lib/installer.sh"
 
-DESTDIR="/usr/src/hid-xpadneo-${VERSION}"
-
 if [[ -z "${INSTALLED[*]}" ]]; then
-
-    echo "* copying module to '${DESTDIR}'"
-    cp --recursive "hid-xpadneo" "${DESTDIR}"
-
-    echo "* replacing version string if necessary"
-    (
-        cd "${DESTDIR}"
-        cp src/version.h.in src/version.h
-        sed -i 's/"@DO_NOT_CHANGE@"/"'"${VERSION}"'"/g' dkms.conf src/version.h
-    )
 
     set -e
 
-    echo "* adding module to DKMS"
-    dkms add "${V[*]}" "hid-xpadneo/${VERSION}" || cat_dkms_make_log
+    echo "* creating dkms.conf"
+    sed 's/"@DO_NOT_CHANGE@"/"'"${VERSION}"'"/g' <hid-xpadneo/dkms.conf.in >hid-xpadneo/dkms.conf
 
     echo "* installing module (using DKMS)"
-    dkms install "${V[*]}" "hid-xpadneo/${VERSION}" || cat_dkms_make_log
+    dkms install "${V[*]}" "hid-xpadneo" --force || cat_dkms_make_log
 
 else
 
