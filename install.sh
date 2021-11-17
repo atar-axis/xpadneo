b3721344ca1b2a3140f5cad7862a18dfac497873
Kai Krakow <kai@kaishome.de>
2021-11-17 05:35:09 +0100

    dkms: Create version instance in DKMS source archives
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- install.sh
+++ install.sh
@@ -11,6 +11,11 @@ if [[ -z "${INSTALLED[*]}" ]]; then
     echo "* creating dkms.conf"
     sed 's/"@DO_NOT_CHANGE@"/"'"${VERSION}"'"/g' <hid-xpadneo/dkms.conf.in >hid-xpadneo/dkms.conf
 
+    # TODO: Works around https://github.com/dell/dkms/issues/177 for DKMS 3
+    echo "* adding hid-xpadneo-${VERSION} folder to /usr/src"
+    mkdir -p "/usr/src/hid-xpadneo-${VERSION}"
+    cp --recursive hid-xpadneo/. "/usr/src/hid-xpadneo-${VERSION}/."
+
     echo "* installing module (using DKMS)"
     dkms install "${V[*]}" "hid-xpadneo" --force || cat_dkms_make_log
 
