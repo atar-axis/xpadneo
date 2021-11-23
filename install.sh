ad45fa872d2d89fcff6c7f9b4c80f0e93e4d3d85
Jerrod Frost <13701401+jcfrosty@users.noreply.github.com>
2021-11-23 13:54:39 -0600

    Update install.sh
    New DKMS causing me grief in Manjaro attempting to compile/install module v0.9.1 . Read up on the manual for a quick fix and update here, only to find there was sort of a fix already here. Figured I could help clean it up some.

--- install.sh
+++ install.sh
@@ -11,10 +11,13 @@ if [[ -z "${INSTALLED[*]}" ]]; then
     echo "* creating dkms.conf"
     sed 's/"@DO_NOT_CHANGE@"/"'"${VERSION}"'"/g' <hid-xpadneo/dkms.conf.in >hid-xpadneo/dkms.conf
 
+    echo "* installing sources (using DKMS)"
     # TODO: Works around https://github.com/dell/dkms/issues/177 for DKMS 3
-    echo "* adding hid-xpadneo-${VERSION} folder to /usr/src"
-    mkdir -p "/usr/src/hid-xpadneo-${VERSION}"
-    cp --recursive "${V[@]}" hid-xpadneo/. "/usr/src/hid-xpadneo-${VERSION}/."
+    cp -a hid-xpadneo/ /usr/src/hid-xpadneo-${VERSION}
+    dkms add -v "hid-xpadneo/${VERSION}" --force || cat_dkms_make_log
+
+    echo "* building modules (using DKMS)"
+    dkms build "hid-xpadneo/${VERSION}" --force || cat_dkms_make_log
 
     echo "* installing module (using DKMS)"
     dkms install "${V[*]}" "hid-xpadneo/${VERSION}" --force || cat_dkms_make_log
