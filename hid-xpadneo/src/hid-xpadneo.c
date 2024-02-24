atar-axis/xpadneo

commit 4c90d749b6e8ee5679a1ba9ae032712491289489

xpadneo, quirks: Combine quirks that mean "no haptics support"
Signed-off-by: Kai Krakow <kai@kaishome.de>

hid-xpadneo/src/hid-xpadneo.c
@@ -87,13 +87,11 @@ struct quirk {
 
 static const struct quirk xpadneo_quirks[] = {
 	DEVICE_OUI_QUIRK("98:B6:EA",
-			 XPADNEO_QUIRK_NO_PULSE | XPADNEO_QUIRK_NO_TRIGGER_RUMBLE |
-			 XPADNEO_QUIRK_REVERSE_MASK),
+			 XPADNEO_QUIRK_NO_HAPTICS | XPADNEO_QUIRK_REVERSE_MASK),
 	DEVICE_OUI_QUIRK("A0:5A:5D",
 			 XPADNEO_QUIRK_NO_PULSE | XPADNEO_QUIRK_NO_MOTOR_MASK),
 	DEVICE_OUI_QUIRK("E4:17:D8",
-			 XPADNEO_QUIRK_NO_PULSE | XPADNEO_QUIRK_NO_TRIGGER_RUMBLE |
-			 XPADNEO_QUIRK_NO_MOTOR_MASK),
+			 XPADNEO_QUIRK_NO_HAPTICS | XPADNEO_QUIRK_NO_MOTOR_MASK),
 };
 
 struct usage_map {

hid-xpadneo/src/xpadneo.h
@@ -67,6 +67,8 @@ do { \
 #define XPADNEO_QUIRK_SHARE_BUTTON      64
 #define XPADNEO_QUIRK_REVERSE_MASK      128
 
+#define XPADNEO_QUIRK_NO_HAPTICS        (XPADNEO_QUIRK_NO_PULSE|XPADNEO_QUIRK_NO_MOTOR_MASK)
+
 /* timing of rumble commands to work around firmware crashes */
 #define XPADNEO_RUMBLE_THROTTLE_DELAY   msecs_to_jiffies(50)
 #define XPADNEO_RUMBLE_THROTTLE_JIFFIES (jiffies + XPADNEO_RUMBLE_THROTTLE_DELAY)

commit e7fc94f5ab82587c756575878346f5c07194c5dc

xpadneo, quirks: Fix the haptics quirks for third-party controllers
Fixes: commit 4c90d74 ("xpadneo, quirks: Combine quirks that mean "no haptics support"")
Maybe-fixes: https://github.com/atar-axis/xpadneo/issues/461
Maybe-fixes: https://github.com/atar-axis/xpadneo/issues/462
Signed-off-by: Kai Krakow <kai@kaishome.de>

hid-xpadneo/src/hid-xpadneo.c
@@ -93,11 +93,10 @@ struct quirk {
 
 static const struct quirk xpadneo_quirks[] = {
 	DEVICE_OUI_QUIRK("98:B6:EA",
-			 XPADNEO_QUIRK_NO_HAPTICS | XPADNEO_QUIRK_REVERSE_MASK),
-	DEVICE_OUI_QUIRK("A0:5A:5D",
-			 XPADNEO_QUIRK_NO_PULSE | XPADNEO_QUIRK_NO_MOTOR_MASK),
-	DEVICE_OUI_QUIRK("E4:17:D8",
-			 XPADNEO_QUIRK_NO_HAPTICS | XPADNEO_QUIRK_NO_MOTOR_MASK),
+			 XPADNEO_QUIRK_NO_PULSE | XPADNEO_QUIRK_NO_TRIGGER_RUMBLE |
+			 XPADNEO_QUIRK_REVERSE_MASK),
+	DEVICE_OUI_QUIRK("A0:5A:5D", XPADNEO_QUIRK_NO_HAPTICS),
+	DEVICE_OUI_QUIRK("E4:17:D8", XPADNEO_QUIRK_NO_HAPTICS | XPADNEO_QUIRK_NO_TRIGGER_RUMBLE),
 };
 
 struct usage_map {