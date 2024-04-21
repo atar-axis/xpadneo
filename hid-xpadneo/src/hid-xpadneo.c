bee865485a414611e50fd902a3992304e9f892aa
yjun <jerrysteve1101@gmail.com>
2024-04-21 17:49:12 +0800

    xpadneo: Fix missing report check
    This fix ensures that the missing_reported flag is correctly updated when the keyboard or gamepad is not detected.
    
    Signed-off-by: Jun Yan <jerrysteve1101@gmail.com>

--- hid-xpadneo/src/hid-xpadneo.c
+++ hid-xpadneo/src/hid-xpadneo.c
@@ -1013,7 +1013,7 @@ combine_z_axes:
 	return 0;
 
 keyboard_missing:
-	if ((xdata->missing_reported && XPADNEO_MISSING_KEYBOARD) == 0) {
+	if ((xdata->missing_reported & XPADNEO_MISSING_KEYBOARD) == 0) {
 		xdata->missing_reported |= XPADNEO_MISSING_KEYBOARD;
 		hid_err(hdev, "keyboard not detected\n");
 	}
@@ -1028,7 +1028,7 @@ static int xpadneo_init_hw(struct hid_device *hdev)
 	struct xpadneo_devdata *xdata = hid_get_drvdata(hdev);
 
 	if (!xdata->gamepad) {
-		if ((xdata->missing_reported && XPADNEO_MISSING_GAMEPAD) == 0) {
+		if ((xdata->missing_reported & XPADNEO_MISSING_GAMEPAD) == 0) {
 			xdata->missing_reported |= XPADNEO_MISSING_GAMEPAD;
 			hid_err(hdev, "gamepad not detected\n");
 		}
