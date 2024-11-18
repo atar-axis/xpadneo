atar-axis/xpadneo

commit d611d7059b7d0456c81df4881c6341c27e637d4d

hid-xpadneo: Allow building with kernel 6.12
Kernel 6.12 changed the signature of a HID function. We can simply
conditionally use the one or the other based on the kernel version.

Fixes: https://github.com/atar-axis/xpadneo/issues/498
Signed-off-by: Kai Krakow <kai@kaishome.de>

hid-xpadneo/src/hid-xpadneo.c
@@ -718,7 +718,11 @@ static int xpadneo_input_mapping(struct hid_device *hdev, struct hid_input *hi,
 	return MAP_AUTO;
 }
 
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6,12,0)
 static u8 *xpadneo_report_fixup(struct hid_device *hdev, u8 *rdesc, unsigned int *rsize)
+#else
+static const u8 *xpadneo_report_fixup(struct hid_device *hdev, u8 *rdesc, unsigned int *rsize)
+#endif
 {
 	struct xpadneo_devdata *xdata = hid_get_drvdata(hdev);
 

commit 746de0f2d1cc334f5f03ed3994555a233b0a46c1

hid-xpadneo: Actually allow building with kernel 6.12
Includes the missing header file.

Fixes: d611d7059b7d0456 ("hid-xpadneo: Allow building with kernel 6.12")
Closes: https://github.com/atar-axis/xpadneo/issues/498
Signed-off-by: Kai Krakow <kai@kaishome.de>

hid-xpadneo/src/hid-xpadneo.c
@@ -10,6 +10,7 @@
 
 #include <linux/delay.h>
 #include <linux/module.h>
+#include <linux/version.h>
 
 #include "xpadneo.h"
 