b27db4621d68fde575f7b33f4ba4e0cdf5e66e70
Kai Krakow <kai@kaishome.de>
2024-11-24 14:22:01 +0100

    hid-xpadneo, core: Match kernel 6.12 report_fixup signature
    This tripped some compilers when promoting warning to errors.
    
    Link: https://github.com/atar-axis/xpadneo/pull/506
    Signed-off-by: Kai Krakow <kai@kaishome.de>

--- hid-xpadneo/src/hid-xpadneo.c
+++ hid-xpadneo/src/hid-xpadneo.c
@@ -722,7 +722,7 @@ static int xpadneo_input_mapping(struct hid_device *hdev, struct hid_input *hi,
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6,12,0)
 static u8 *xpadneo_report_fixup(struct hid_device *hdev, u8 *rdesc, unsigned int *rsize)
 #else
-static const u8 *xpadneo_report_fixup(struct hid_device *hdev, u8 *rdesc, unsigned int *rsize)
+static const __u8 *xpadneo_report_fixup(struct hid_device *hdev, __u8 *rdesc, unsigned int *rsize)
 #endif
 {
 	struct xpadneo_devdata *xdata = hid_get_drvdata(hdev);
