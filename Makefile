atar-axis/xpadneo

commit b418e9fa07a804ee08f40046fdf578bf959571fd

xpadneo, installer: Introduce make-based installer
This adds a new top-level `Makefile` for easy installation via package
managers. Instead of directly building the DKMS module, this allows
a package installer to just provide the source code and install the
support files. Building of the actual module via DKMS is left to the
package maintainer.

To aid following packaging guidelines, two variables are provided:

- `PREFIX`: Allows to install the package to a pre-install image which
  can be put in an archive for package deployment and file tracking. If
  this variable is provided, it will also skip deployment of the DKMS
  source tree. This is mainly due to limitations of DKMS handling
  prefixed installations properly. Package maintainers have to do this
  manually in a post-installation step.

- `ETC_PREFIX`: Allows to install files to `/usr/lib` which would
  normally go to `/etc`. This is applied relative to `PREFIX`.

TL;DR: `dkms add` won't be run if `PREFIX` is provided.

Please read `docs/PACKAGING.md`.

Maybe-fixes: https://github.com/atar-axis/xpadneo/issues/405
Maybe-fixes: https://github.com/atar-axis/xpadneo/issues/425
Maybe-fixes: https://github.com/atar-axis/xpadneo/issues/431
Maybe-fixes: https://github.com/atar-axis/xpadneo/issues/491
Signed-off-by: Kai Krakow <kai@kaishome.de>

Makefile
@@ -1,0 +1,49 @@
+MODPROBE_CONFS := xpadneo.conf
+UDEV_RULES := 60-xpadneo.rules 70-xpadneo-disable-hidraw.rules
+
+ifeq ($(PREFIX),)
+  DKMS ?= dkms
+else
+  $(warning Installing to prefix, dkms commands will not be run!)
+  DKMS ?= : SKIPPING dkms
+endif
+
+ETC_PREFIX ?= $(PREFIX)/etc
+
+all: build
+
+help:
+	@echo "Targets:"
+	@echo "help        This help"
+	@echo "build       Prepare the package for DKMS deployment"
+	@echo "install     Install the package and DKMS source code"
+	@echo "uninstall   Uninstall the package and DKMS source code"
+	@echo
+	@echo "Variables:"
+	@echo "PREFIX      Install files into this prefix"
+	@echo "ETC_PREFIX  Install etc files into this prefix (defaults to \$PREFIX/etc)"
+	@echo
+	@echo "Using PREFIX requires handling dkms commands in your package script."
+
+.PHONY: build install
+
+.INTERMEDIATE: VERSION
+
+VERSION:
+	{ [ -n "$(VERSION)" ] && echo $(VERSION) || git describe --tags --dirty; } >$@
+
+build: VERSION
+	$(MAKE) VERSION="$(shell cat VERSION)" -C hid-xpadneo dkms.conf
+
+install: build
+	mkdir -p $(PREFIX)/etc/modprobe.d $(PREFIX)/etc/udev/rules.d
+	install -D -m 0644 -t $(PREFIX)/etc/modprobe.d $(MODPROBE_CONFS:%=hid-xpadneo/etc-modprobe.d/%)
+	install -D -m 0644 -t $(PREFIX)/etc/udev/rules.d $(UDEV_RULES:%=hid-xpadneo/etc-udev-rules.d/%)
+	$(DKMS) add hid-xpadneo
+
+uninstall: VERSION
+	$(DKMS) remove "hid-xpadneo/$(shell cat VERSION)" --all
+	rm -Rf "$(PREFIX)/usr/src/hid-xpadneo-$(shell cat VERSION)"
+	rm -f $(PREFIX)$(UDEV_RULES:%=$(ETC_PREFIX)/udev/rules.d/%)
+	rm -f $(PREFIX)$(MODPROBE_CONFS:%=$(ETC_PREFIX)/modprobe.d/%)
+	rmdir --ignore-fail-on-non-empty -p $(PREFIX)/etc/modprobe.d $(PREFIX)/etc/udev/rules.d

hid-xpadneo/Makefile
@@ -6,7 +6,9 @@ all: modules
 .INTERMEDIATE: ../VERSION
 
 ../VERSION:
-	git describe --tags --dirty >$@
+	$(MAKE) -C .. $(@:../%=%)
+
+# convenience rules for local dvelopment
 
 clean modules modules_install: ../VERSION
 	$(MAKE) -C $(KERNEL_SOURCE_DIR) INSTALL_MOD_DIR="kernel/drivers/hid" LD=$(LD) M=$(shell pwd)/src VERSION="$(shell cat ../VERSION)" $@
@@ -15,6 +17,8 @@ reinstall: modules
 	sudo make modules_install
 	sudo rmmod hid-xpadneo || true
 	sudo modprobe hid-xpadneo $(MOD_PARAMS)
+
+# DKMS support rules
 
 dkms.conf: dkms.conf.in ../VERSION
 	sed 's/"@DO_NOT_CHANGE@"/"$(shell cat ../VERSION)"/g' <"$<" >"$@"

lib/installer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[ -n "${PREFIX}" ] && { echo >&2 "ERROR: Prefix installations are not supported, use make instead."; exit 1; }
+
 if [ ${EUID} -ne 0 ]; then
 	echo >&2 "ERROR: You most probably need superuser privileges to use this script, please run me via sudo!"
 	exit 3

commit dbd387c7c9bce1276ffe7456c3b64fb5cc213ba9

xpadneo, dkms: Get rid of redundant file installs/removes
The module configuration files are handled by the package. Dynamic
rebuilds of the module for each kernel version must not handle these
files, otherwise the installation breaks on kernel remove.

Since we are no longer handling any udev rules in DKMS, let's also move
calls to udevadm out of the hooks. This really should be handled by the
package manager, and thus we can now properly skip udevadm during
prefix installations, too.

Fixes: b418e9fa07a8 ("xpadneo, installer: Introduce make-based installer")
See-also: https://github.com/atar-axis/xpadneo/issues/521
Signed-off-by: Kai Krakow <kai@kaishome.de>

Makefile
@@ -13,6 +13,13 @@ else
   DKMS ?= : SKIPPING dkms
 endif
 
+ifeq ($(PREFIX),)
+  UDEVADM ?= udevadm
+else
+  $(warning Installing to prefix, udevadm commands will not be run!)
+  UDEVADM ?= : SKIPPING udevadm
+endif
+
 all: build
 
 help:
@@ -44,6 +51,7 @@ install: build
 	install -D -m 0644 -t $(PREFIX)$(ETC_PREFIX)/modprobe.d $(MODPROBE_CONFS:%=hid-xpadneo/etc-modprobe.d/%)
 	install -D -m 0644 -t $(PREFIX)$(ETC_PREFIX)/udev/rules.d $(UDEV_RULES:%=hid-xpadneo/etc-udev-rules.d/%)
 	install -D -m 0644 -t $(PREFIX)$(DOC_PREFIX) $(DOC_SRCS)
+	$(UDEVADM) control --reload
 	$(DKMS) add hid-xpadneo
 
 uninstall: VERSION
@@ -53,3 +61,4 @@ uninstall: VERSION
 	rm -f $(UDEV_RULES:%=$(PREFIX)$(ETC_PREFIX)/udev/rules.d/%)
 	rm -f $(MODPROBE_CONFS:%=$(PREFIX)$(ETC_PREFIX)/modprobe.d/%)
 	rmdir --ignore-fail-on-non-empty -p $(PREFIX)$(ETC_PREFIX)/modprobe.d $(PREFIX)$(ETC_PREFIX)/udev/rules.d $(PREFIX)$(DOC_PREFIX)
+	$(UDEVADM) control --reload

hid-xpadneo/dkms.post_install
@@ -3,13 +3,3 @@
 # DKMS now defaults to kernel 5.12 minimum, remove ERTM override
 ERTM_OVERRIDE="/etc/modprobe.d/99-xpadneo-bluetooth.conf"
 [ -L "${ERTM_OVERRIDE}" ] || { echo "Forcing removal of ERTM override..."; rm -f "${ERTM_OVERRIDE}"; }
-
-echo "Installing modalias database..."
-install -D -m 0644 -t /etc/modprobe.d etc-modprobe.d/xpadneo.conf
-
-echo "Installing udev rules..."
-install -D -m 0644 -t /etc/udev/rules.d etc-udev-rules.d/60-xpadneo.rules
-install -D -m 0644 -t /etc/udev/rules.d etc-udev-rules.d/70-xpadneo-disable-hidraw.rules
-
-echo "Reloading udev..."
-udevadm control --reload

hid-xpadneo/dkms.post_remove
@@ -3,13 +3,3 @@
 # DKMS now defaults to kernel 5.12 minimum, remove ERTM override
 ERTM_OVERRIDE="/etc/modprobe.d/99-xpadneo-bluetooth.conf"
 [ -L "${ERTM_OVERRIDE}" ] || { echo "Uninstalling ERTM override..."; rm -f "${ERTM_OVERRIDE}"; }
-
-echo "Uninstalling modalias database..."
-rm -f /etc/modprobe.d/xpadneo.conf
-
-echo "Uninstalling udev rules..."
-rm -f /etc/udev/rules.d/70-xpadneo-disable-hidraw.rules
-rm -f /etc/udev/rules.d/60-xpadneo.rules
-
-echo "Reloading udev..."
-udevadm control --reload