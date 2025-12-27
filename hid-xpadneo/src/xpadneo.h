atar-axis/xpadneo

commit 6168033e3d6c3c1c57072e1953248ccf6ad0a89f

xpadneo: Remove deprecated ida_simple_{get,remove}() usage
Update to use ida_alloc() and ida_free() in place of the deprecated
ida_simple_get() and ida_simple_remove() calls. Linux has removed this
API in the v6.18 release candidates. ida_simple_xx() has been
deprecated since v5.10 and the new API was introduced in v4.19.

Co-authored-by: orderedstereographic <161899146+orderedstereographic@users.noreply.github.com>
Fixes: https://github.com/atar-axis/xpadneo/pull/562
Closes: https://github.com/atar-axis/xpadneo/pull/566
Signed-off-by: Kai Krakow <kai@kaishome.de>

hid-xpadneo/src/xpadneo.h
@@ -16,7 +16,11 @@
 
 #include "hid-ids.h"
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4,18,0)
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 0)
+#error "kernel version 4.19.0+ required for ida_simple_{alloc,free}()"
+#endif
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 18, 0)
 #error "kernel version 4.18.0+ required for HID_QUIRK_INPUT_PER_APP"
 #endif
 

hid-xpadneo/src/hid-xpadneo.c
@@ -1218,7 +1218,7 @@ static int xpadneo_probe(struct hid_device *hdev, const struct hid_device_id *id
 	if (xdata == NULL)
 		return -ENOMEM;
 
-	index = ida_simple_get(&xpadneo_device_id_allocator, 0, 0, GFP_KERNEL);
+	index = ida_alloc(&xpadneo_device_id_allocator, GFP_KERNEL);
 	if (index < 0)
 		return index;
 	xdata->id = index;
@@ -1327,7 +1327,7 @@ static int xpadneo_probe(struct hid_device *hdev, const struct hid_device_id *id
 static void xpadneo_release_device_id(struct xpadneo_devdata *xdata)
 {
 	if (xdata->id >= 0) {
-		ida_simple_remove(&xpadneo_device_id_allocator, xdata->id);
+		ida_free(&xpadneo_device_id_allocator, xdata->id);
 		xdata->id = -1;
 	}
 }

commit a4ed5ad4912dd049fc1e3d049e717b31d73fbdc7

xpadneo, core: Remove hard requirement of ida_{alloc,free}
Instead of failing compilation with older kernels, alias the functions
that have been replaced in kernel 4.19+.

Partially reverts commit 6168033e3d6c3c1c57072e1953248ccf6ad0a89f.
This reverts commit 6ce8de21da2d1d72881bacfde7eb30a003d49faf.

Fixes: 6168033e3d6c ("xpadneo: Remove deprecated ida_simple_{get,remove}() usage")
Signed-off-by: Kai Krakow <kai@kaishome.de>

hid-xpadneo/src/xpadneo.h
@@ -16,8 +16,14 @@
 
 #include "hid-ids.h"
 
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 19, 0)
-#error "kernel version 4.19.0+ required for ida_simple_{alloc,free}()"
+/* v4.19: ida_simple_{get,remove}() have been replaced */
+#if KERNEL_VERSION(4, 19, 0) >= LINUX_VERSION_CODE
+#define ida_alloc(a, f) ida_simple_get((a), 0, 0, (f))
+#define ida_free(a, f) ida_simple_remove((a), (f))
+#endif
+
+#if KERNEL_VERSION(4, 18, 0) > LINUX_VERSION_CODE
+#error "kernel version 4.18.0+ required for HID_QUIRK_INPUT_PER_APP"
 #endif
 
 #ifndef VERSION