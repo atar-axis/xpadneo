atar-axis/xpadneo

commit 3b264fc0e3aae43c6b19c6aed105e3642cced22c

xpadneo, core: Add ASUS ROG Raikiri Pro support
The ASUS ROG Raikiri Pro uses a HID descriptor compatible with the
modern Xbox Wireless Controller over Bluetooth, including the same
rumble output report.

Add the ASUS VID/PID to the Bluetooth device table and skip descriptor
heuristics for this device. Its randomized Bluetooth MAC address can
otherwise match the GameSir heuristic and disable rumble through
simple-clone quirks.

Document the shared 283-byte descriptor and record why the GameSir
heuristic must not apply to this device.

Fixes: https://github.com/atar-axis/xpadneo/issues/612
Signed-off-by: Kai Krakow <kai@kaishome.de>

hid-xpadneo/src/xpadneo/core.c
@@ -39,6 +39,10 @@ static DEFINE_IDA(xpadneo_core_device_id_allocator);
 #define USB_VENDOR_ID_MICROSOFT 0x045e
 #endif
 
+#ifndef USB_VENDOR_ID_ASUSTEK
+#define USB_VENDOR_ID_ASUSTEK 0x0b05
+#endif
+
 /* driver_data stores XPADNEO_DEVFLAG_* flags */
 static const struct hid_device_id core_devices[] = {
 	/* XBOX ONE S / X */
@@ -55,6 +59,10 @@ static const struct hid_device_id core_devices[] = {
 	/* XBOX Series X|S / Xbox Wireless Controller (BLE) */
 	{ HID_BLUETOOTH_DEVICE(USB_VENDOR_ID_MICROSOFT, 0x0B13),
 	 .driver_data = XPADNEO_DEVFLAG_CAP_SHARE_BUTTON },
+
+	/* ASUS ROG Raikiri Pro */
+	{ HID_BLUETOOTH_DEVICE(USB_VENDOR_ID_ASUSTEK, 0x1ABD),
+	 .driver_data = XPADNEO_DEVFLAG_SKIP_HEURISTICS },
 
 	/* SENTINEL VALUE, indicates the end */
 	{ }
@@ -150,6 +158,9 @@ static int core_probe(struct hid_device *hdev, const struct hid_device_id *id)
 
 	xdata->quirks = 0;
 	xdata->device_flags = id->driver_data;
+
+	if (xdata->device_flags & XPADNEO_DEVFLAG_SKIP_HEURISTICS)
+		xdata->quirks |= XPADNEO_QUIRK_NO_HEURISTICS;
 
 	xdata->hdev = hdev;
 	hdev->quirks |= HID_QUIRK_INPUT_PER_APP;

hid-xpadneo/src/xpadneo/xpadneo.h
@@ -52,6 +52,7 @@
 
 /* HID device flags stored in hid_device_id.driver_data */
 #define XPADNEO_DEVFLAG_CAP_SHARE_BUTTON BIT(0)
+#define XPADNEO_DEVFLAG_SKIP_HEURISTICS  BIT(1)
 
 /* maximum length of report 0x01 for duplicate packet filtering */
 #define XPADNEO_REPORT_0x01_LENGTH (55+1)

commit 136ae14c96296012de6152ef0f1f952d659df2f4

xpadneo, core: Spoof vendor with controller IDs
When exposing the patched Windows-style mapping, xpadneo rewrites the
controller product and version so user space sees the expected
Xbox One S wireless identity.

Also rewrite and restore the vendor ID so third-party controllers that
reuse Xbox descriptors, such as the ASUS ROG Raikiri Pro, do not end up
with a mixed ASUS vendor and Microsoft product identity.

Fixes: 3b264fc0e3aa ("xpadneo, core: Add ASUS ROG Raikiri Pro support")
Signed-off-by: Kai Krakow <kai@kaishome.de>

hid-xpadneo/src/xpadneo/core.c
@@ -119,11 +119,14 @@ static void core_remove(struct hid_device *hdev)
 		hdev->version = xdata->original_version;
 	}
 
-	if (hdev->product != xdata->original_product) {
+	if ((hdev->vendor != xdata->original_vendor) ||
+	    (hdev->product != xdata->original_product)) {
 		hid_info(hdev,
-			 "reverting to original product "
-			 "(changed PID from 0x%04X to 0x%04X)\n",
+			 "reverting to original vendor/product "
+			 "(changed VID from 0x%04X to 0x%04X, PID from 0x%04X to 0x%04X)\n",
+			 hdev->vendor, xdata->original_vendor,
 			 hdev->product, xdata->original_product);
+		hdev->vendor = xdata->original_vendor;
 		hdev->product = xdata->original_product;
 	}
 
@@ -223,22 +226,19 @@ static int core_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	 * still detect our driver as the correct model. Currently this
 	 * maps all controllers to the same model.
 	 */
+	xdata->original_vendor = hdev->vendor;
 	xdata->original_product = hdev->product;
 	xdata->original_version = hdev->version;
+	hdev->vendor = USB_VENDOR_ID_MICROSOFT;
 	hdev->product = 0x028E;
 	hdev->version = 0x00001130;
 
-	if (hdev->product != xdata->original_product)
+	if ((hdev->vendor != xdata->original_vendor) || (hdev->product != xdata->original_product))
 		hid_info(hdev,
 			 "pretending XB1S Windows wireless mode "
-			 "(changed PID from 0x%04X to 0x%04X)\n", xdata->original_product,
+			 "(changed VID from 0x%04X to 0x%04X, PID from 0x%04X to 0x%04X)\n",
+			 xdata->original_vendor, hdev->vendor, xdata->original_product,
 			 hdev->product);
-
-	if (hdev->version != xdata->original_version)
-		hid_info(hdev,
-			 "working around wrong SDL2 mappings "
-			 "(changed version from 0x%08X to 0x%08X)\n", xdata->original_version,
-			 hdev->version);
 
 	ret = hid_parse(hdev);
 	if (ret) {
@@ -295,6 +295,7 @@ err_stop_hw:
 
 err_release_id:
 	/* restore the original device IDs first */
+	hdev->vendor = xdata->original_vendor;
 	hdev->product = xdata->original_product;
 	hdev->version = xdata->original_version;
 

hid-xpadneo/src/xpadneo/xpadneo.h
@@ -129,6 +129,7 @@ struct xpadneo_devdata {
 	short int missing_reported;
 
 	/* revert fixups on removal */
+	u16 original_vendor;
 	u16 original_product;
 	u32 original_version;
 